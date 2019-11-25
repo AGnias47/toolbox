adc7945ad62f0f1c25c074333394b627b048eedc
Andy Gnias <AGnias47@gmail.com>
2019-11-24 22:28:21 -0500

    Add more C functions

--- C/basic_func.c
+++ C/basic_func.c
@@ -5,6 +5,8 @@
 
    Libraries Used:
                   stdio.h - printf and scanf
+						stdlib.h - free and malloc
+						time.h - timer
 
    Inputs: User input required for some functions
    Return: None; printf statements for each function
@@ -15,6 +17,13 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/*pseudo bool type*/
+typedef int Bool;
+enum{false, true};
 
 
 /*
@@ -78,14 +87,64 @@ void swap(int *a, int *b)
 }
 
 
+/*
+	Shows how an array can function as a pointer
+
+	@params: none
+	@return: None
+*/
+void array_as_pointer()
+{
+	int A[5];
+	int *p;
+	*A = 12;
+	*(A+3) = 5;
+	for (p=A; p<A+(sizeof(A)/sizeof(A[0])); p++) printf("%d\n", *p);
+}
+
+
+/*
+	Returns a reversed array of ints using a for loop
+	@param A: array to reverse (not mutated)
+	@param size: size of array
+	@return R: reversed array
+*/
+int * reverse_array(int * A, int size)
+{
+	if (A == NULL) return A;
+	int * R = malloc(sizeof(int) * size);
+	for (int i=size-1, j=0; i >= 0; i--, j++)
+	{
+		R[j] = A[i];
+	}
+	return R;
+}
+
+/*
+	Shows timer functionality
+	@params: None
+	@return: None
+*/
+void timer()
+{
+	clock_t before;
+	double elapsed;
+	before = clock();
+	int twentyfive = 5*5;
+	elapsed = clock() - before;
+	printf("Program took %.3f seconds to run\n",
+			elapsed/CLOCKS_PER_SEC);
+}
+
+
 int main()
 {
    printf("---printing---\n");
    printing();
    printf("---iterators---\n");
    iterators();
-   printf("---read_array---\n");
-   read_array();
+   /*printf("---read_array---\n");
+   read_array();*/
    printf("---swap---\n");
    int a = 42;
    int b = 47;
@@ -93,5 +152,13 @@ int main()
    printf("Running swap function\n");
    swap(&a,&b);
    printf("a is now %d and b is now %d\n",a,b);
+	printf("---Arrays as pointers---\n");
+   array_as_pointer();
+	int rev[] = {21, 32, 43};
+	int *B = reverse_array(rev, 3);
+   for (int i=0; i < 3; i++) printf("%d\n", B[i]);
+	timer();
    return 0;
 }
+
+
