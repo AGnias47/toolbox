f83a67c6412176720836afd3184ecb89e5992437
Andy Gnias <AGnias47@gmail.com>
2019-12-28 22:30:34 -0500

    Add arithmetic function

--- rust/basic_functionality.rs
+++ rust/basic_functionality.rs
@@ -2,3 +2,8 @@ pub fn print_string(s: String)
 {
 	println!("{}", s);
 }
+
+pub fn cube(a: u32) -> u32
+{
+	return a.pow(3);
+}
--- rust/main.rs
+++ rust/main.rs
@@ -3,4 +3,6 @@ mod basic_functionality;
 fn main()
 {
 	basic_functionality::print_string(String::from("Hello World!"));
+	let a = basic_functionality::cube(5);
+	println!("{}", a);
 }
