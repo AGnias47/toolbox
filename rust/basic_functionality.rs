bb41a7d94a35e26428c3fbfc48295871cfe76624
Andy Gnias <AGnias47@gmail.com>
2020-03-22 18:50:41 -0400

    Add additional rust functions Update makefile

--- rust/basic_functionality.rs
+++ rust/basic_functionality.rs
@@ -1,9 +1,28 @@
 pub fn print_string(s: String)
 {
-	println!("{}", s);
+    println!("{}", s);
 }
 
 pub fn cube(a: u32) -> u32
 {
-	return a.pow(3);
+    return a.pow(3);
 }
+
+pub fn get_float_quotient(numerator: i32, denominator: i32) -> f32
+{
+    let numerator = numerator as f32;
+    let denominator = denominator as f32;
+    return numerator / denominator;
+}
+
+pub fn is_prime(n: i32) -> bool {
+    let upper_limit: i32 = (n as f64).sqrt() as i32 + 1;
+    for i in 2..upper_limit {
+        if n % i == 0 { 
+            return false;
+        }
+    }
+    return true;
+}
+
+
--- rust/main.rs
+++ rust/main.rs
@@ -2,7 +2,14 @@ mod basic_functionality;
 
 fn main()
 {
-	basic_functionality::print_string(String::from("Hello World!"));
-	let a = basic_functionality::cube(5);
-	println!("{}", a);
+    basic_functionality::print_string(String::from("Hello World!"));
+    let a = basic_functionality::cube(5);
+    println!("{}", a);
+    let b = basic_functionality::get_float_quotient(7, 5);
+    println!("{}", b);
+    let c = 14;
+    let d: i32 = 17;
+    let e = basic_functionality::is_prime(c);
+    let f = basic_functionality::is_prime(d);
+    println!("{}, {}", e, f);   
 }
