872f10bb49eb60e1039c67e1245a799c8d536509
Andy Gnias <AGnias47@gmail.com>
2020-10-05 21:00:08 -0400

    Better implementation

--- python/basic_ciphers.py
+++ python/basic_ciphers.py
@@ -2,16 +2,17 @@
 
 """
 Cesar Cipher - Assigns ints to each letter; key encrypts by moving string n spaces, where n is the integer encoding of
-the key.
+the key. Implementation is imperfect but prevents us from having to create a full dict mapping of all characters. Works
+for standard english characters, which is the main purpose of the cipher.
 """
 
 
 def cesar_cipher(body, key, action="encrypt"):
     key_val = char_to_int(key)
     if action == "encrypt":
-        return "".join([chr(char_to_int(c) + key_val + 64) for c in body])
+        return "".join([chr((char_to_int(c) + key_val) % 26 + 64) for c in body])
     elif action == "decrypt":
-        return "".join([chr(char_to_int(c) - key_val + 64) for c in body])
+        return "".join([chr((char_to_int(c) - key_val) % 26 + 64) for c in body])
     raise ValueError
 
 
@@ -21,7 +22,7 @@ def char_to_int(c):
 
 if __name__ == "__main__":
     key = "E"
-    encrypted = cesar_cipher("Hello World!", key, "encrypt")
+    encrypted = cesar_cipher("HelloWorld", key, "encrypt")
     print(encrypted)
     decrypted = cesar_cipher(encrypted, key, "decrypt")
     print(decrypted)
