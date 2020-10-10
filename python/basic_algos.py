9b94d7f5d185e6fefebca1573b26d8ce4404f3f8
Andy Gnias <andrew.gnias@stratagemgroup.com>
2020-10-10 10:33:49 -0400

    updates

--- python/basic_algos.py
+++ python/basic_algos.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 """
-Cesar Cipher - Assigns ints to each letter; key encrypts by moving string n spaces, where n is the integer encoding of
-the key. Implementation is imperfect but prevents us from having to create a full dict mapping of all characters. Works
-for standard english characters, which is the main purpose of the cipher.
-
 Vigenere Cipher - Same as Cesar except the key is a word that is repeated over the text
 
 Kasiski Algorithm - Used to crack Vigenere Cipher. Find a repeated substring to determine the length 
@@ -72,22 +68,9 @@ def otp(text, key):
     return "".join(otp_coding)
 
 
-def cesar_cipher(body, key, action="encrypt"):
-    key_val = char_to_int(key)
-    if action == "encrypt":
-        return "".join([chr((char_to_int(c) + key_val) % 26 + 64) for c in body])
-    elif action == "decrypt":
-        return "".join([chr((char_to_int(c) - key_val) % 26 + 64) for c in body])
-    raise ValueError
-
-
-def char_to_int(c):
-    return ord(c.upper()) - 64
-
-
 if __name__ == "__main__":
-    key = "E"
-    encrypted = cesar_cipher("HelloWorld", key, "encrypt")
+    key = "XHSLE"
+    encrypted = otp("HELLO", key)
     print(encrypted)
-    decrypted = cesar_cipher(encrypted, key, "decrypt")
+    decrypted = otp(encrypted, key)
     print(decrypted)
