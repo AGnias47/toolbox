6c368ab9dd9419d7483ed82917b2b09251acabef
Andy Gnias <AGnias47@gmail.com>
2021-01-23 23:14:32 -0500

    Update factorial

--- C++/GoogleTestExample/factorial.cpp
+++ C++/GoogleTestExample/factorial.cpp
@@ -2,17 +2,40 @@
 
 int factorial(int n)
 {
-    if (n < 1)
+    if (n < 0)
     {
-        throw "Must be > 0";
+        throw "Must be >= 0";
     }
-    if (n == 1)
+    else if (n == 0 || n == 1)
     {
         return n;
     }
     else
     {
         return n * factorial(n - 1);
+    }
+}
+
+
+int factorial(int n, std::vector<int>& M)
+{
+    if (n < 0)
+    {
+        throw "Must be >= 0";
+    }
+    else if (M[n] != 0)
+    {
+        return M[n];
+    }
+    else if (n == 0 || n == 1)
+    {
+        M[n] = 1;
+        return 1;
+    }
+    else
+    {
+        M[n] = n * factorial(n - 1);
+        return M[n];
     }
 }
 
