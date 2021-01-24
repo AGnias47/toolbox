bc7c167c7afd3ff10e12025fde3f26c73acedf5d
Andy Gnias <AGnias47@gmail.com>
2021-01-23 23:13:28 -0500

    Add factorial implementations

---
+++ C++/factorial.cpp
@@ -0,0 +1,45 @@
+#include "factorial.hpp"
+
+int factorial(int n)
+{
+    if (n < 0)
+    {
+        throw "Must be >= 0";
+    }
+    else if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+
+int factorial(int n, std::vector<int>& M)
+{
+    if (n < 0)
+    {
+        throw "Must be >= 0";
+    }
+    else if (M[n] != 0)
+    {
+        return M[n];
+    }
+    else if (n == 0 || n == 1)
+    {
+        M[n] = 1;
+        return 1;
+    }
+    else
+    {
+        M[n] = n * factorial(n - 1);
+        return M[n];
+    }
+}
+
