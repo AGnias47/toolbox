fdcf66f02ef711aa25887b3fc29e574fb1f4bd6f
Andy Gnias <AGnias47@gmail.com>
2021-10-09 20:21:39 -0400

    Queue now working

--- data_structures/array_based_queue.cpp
+++ data_structures/array_based_queue.cpp
@@ -1,26 +1,46 @@
-#include "queue.hpp"
+#include "array_based_queue.hpp"
 
-Queue::Queue() : DynamicSet()
+/**
+ * @brief Construct a new Array Based Queue:: Array Based Queue object
+ * This implementation mainly proves to show why an array-based queue is 
+ * a bad idea. In a stack, using an array makes sense because items being 
+ * removed from the stack are getting popped at the end of the list. When 
+ * a resize occurs, there's no inefficiencies in the space being used in 
+ * the original array
+ * 
+ * In a queue, items are dequeued from the front of the array. If nothing 
+ * is done to reuse this space, as is done in this implementation, the 
+ * amount of unused space will approach infinity. There are techniques to 
+ * avoid this, such as reusing the space at the beginning of the array 
+ * once items are dequeued. However, this requires either the array to 
+ * be restructured at some point, or to add additional tracking indicies 
+ * to ensure the right items are dequeued at the right time. 
+ * 
+ */
+ArrayBasedQueue::ArrayBasedQueue() : DynamicSet()
 {
     add_index = 0;
 }
 
-void Queue::add(int n)
+/**
+ * @brief Add to the queue
+ * 
+ * @param n Item to add
+ */
+void ArrayBasedQueue::add(int n)
 {
-    if (add_index == index || ((add_index) == size && index == 0))
-    {
-        add_index = size;
-        resize();
-    }
-    if ((add_index) == size && index != 0)
-        add_index = 0;
-    if (index == -1)
-        index = 0;
+    if (add_index == size) resize();
+    if (index == -1) index = 0;
     data[add_index] = n;
     add_index++;
 }
 
-int Queue::pop()
+/**
+ * @brief Pop an item from the queue
+ * 
+ * @return int Item dequeued
+ */
+int ArrayBasedQueue::pop()
 {
     index++;
     return data[index - 1];
