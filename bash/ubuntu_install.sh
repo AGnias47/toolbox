456ed8f61a889564b6e9864ea6c949902cb1a47c
Andy Gnias <AGnias47@gmail.com>
2022-09-25 21:54:23 -0400

    Install docker

--- bash/ubuntu_install.sh
+++ bash/ubuntu_install.sh
@@ -5,7 +5,7 @@ Installs commonly used utilities for Ubuntu. This is mainly for me so it may
 contain excessive installs for someone elses preferences
 '
 
-# Install aliasfile
+# Install aliasfile and .vimrc
 wget -P ~/.bash_aliases https://raw.githubusercontent.com/AGnias47/toolbox/main/bash/aliasfile
 wget -P ~/.vimrc https://raw.githubusercontent.com/AGnias47/toolbox/main/bash/vimrc
 
@@ -21,10 +21,12 @@ gcc \
 g++ \
 python3-{dev,venv,pip} \
 openjdk-17-jdk-headless \
+ruby-full \
+uidmap \
 cmatrix
 
 # Install packages via snap
-sudo snap install spotify pycharm-community
+sudo snap install spotify
 sudo snap install pycharm-community --classic
 sudo snap install brave
 sudo snap install gimp
@@ -36,7 +38,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 nvm install node
 # Install vscode
-sudo apt-get install wget gpg
+sudo apt install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -45,16 +47,28 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
 
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+dockerd-rootless-setuptool.sh install
+rm get-docker.sh
+
+# Install mdless markdown formatter
+sudo gem install mdless
+
 # Create bash profile
-echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bash_profile
+echo 'export PATH=$PATH:/usr/local/bin:/home/$USER/.local/bin' >> ~/.bash_profile
 echo 'source ~/.bash_profile || true' >> ~/.bashrc
+source ~/.bashrc
 
 # Install and run aptupdate script
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/AGnias47/toolbox/main/utilities/aptupdate
 sudo chmod 755 /usr/local/bin/aptupdate
+echo 'export DOCKER_HOST=unix:///run/user/1000/docker.sock' >> ~/.bash_profile
 sudo aptupdate
 
 echo "Manual Steps to take"
 echo "- Setup ssh keys"
 echo "- Install Dropbox"
+echo "- Confirm rootless Docker install"
 
