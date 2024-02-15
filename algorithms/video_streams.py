035c1bb4f80e5dfc5579d187e613578a2d28a1e8
Andy Gnias <AGnias47@gmail.com>
2024-02-14 19:36:08 -0500

    Video stream algorithm

--- algorithms/video_streams.py
+++ algorithms/video_streams.py
@@ -10,23 +10,33 @@ class Stream:
     def __init__(self, bits, seconds):
         self.bits = bits
         self.seconds = seconds
+        self.bit_rate = self.bits / self.seconds
 
+    def __repr__(self):
+        return f"Stream: {self.bits} bits over {self.seconds} seconds"
 
-def is_valid(schedule, r=5000):
+    def __lt__(self, o):
+        return self.bit_rate < o.bit_rate
+
+def schedule(streams, r=5000):
+    sorted_streams = sorted(streams)
     t = 0
     b = 0
-    for s in schedule:
-        b_s = s.bits / s.seconds
+    for s in sorted_streams:
         for i in range(s.seconds):
-            b += s.bits
-            t += s.seconds
+            b += s.bit_rate
+            t += 1
             if b > r * t:
-                return False
-    return True
+                return []
+    return sorted_streams 
 
 
 s1 = Stream(2000, 1)
 s2 = Stream(6000, 2)
 s3 = Stream(2000, 1)
 
-print(is_valid([s1, s2, s3]))
+print(schedule([s1, s2, s3]))
+
+# n (calc bit_rate) + nlogn (sort) + (n + i)
+# algo is dominated by the sort
+# i is at minimum equal to n if each n has a time period of 1. Else it is greater so
