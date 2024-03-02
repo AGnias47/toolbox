1647a849a0a95ea93f9f51b651ab30b1222a4051
Andy Gnias <AGnias47@gmail.com>
2024-03-02 14:50:06 -0500

    HW2 algos

---
+++ algorithms/hw53.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+S = [1,2,2,3,3,3,4,5]
+
+def create_set(S):
+    if len(S) == 1:
+        return S
---
+++ algorithms/low_stress_high_stress.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+SENTINEL = 0
+
+low_stress = [SENTINEL, 10, 1, 10, 10]
+high_stress = [SENTINEL, 5, 50, 5, 1]
+n = len(low_stress) - 1
+M = [None] * (n + 1)
+
+
+def plan(n, next_job_high=False):
+    if n == 1:
+        if next_job_high:
+            return 0
+        M[n] = max(low_stress[n], high_stress[n])
+        return M[n]
+    if next_job_high:
+        return 0 + plan(n - 1, False)
+    if M[n]:
+        return M[n]
+    if low_stress[n] > high_stress[n]:
+        M[n] = low_stress[n] + plan(n - 1, False)
+    else:
+        M[n] = high_stress[n] + +plan(n - 1, True)
+    return M[n]
+
+
+if __name__ == "__main__":
+    print(plan(n))
+    print(M)
