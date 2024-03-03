1bdbaeda1c394f7b8bfeb553d40530c6cef446b1
Andy Gnias <AGnias47@gmail.com>
2024-03-03 13:08:44 -0500

    Working

--- algorithms/longest_graph_path.py
+++ algorithms/longest_graph_path.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+"""
+Meant to find the longest path in a directed, ordered graph. This is a very simple graph
+implementation. Each node is represented by a list. The first element of the list is the
+node's value, and the rest of the elements are the nodes that the current node points
+to. The graph is represented by a list of nodes. The first element of the list is a
+sentinel value, and the rest of the elements are the nodes in the graph. M is the
+memoization array.
+"""
+
 v1 = [1, 2, 3]
 v2 = [2, 5]
 v3 = [3, 4]
@@ -7,6 +16,7 @@ v4 = [4, 5]
 v5 = [5]
 SENTINEL = 0
 available_nodes = [SENTINEL, v1, v2, v3, v4, v5]
+M = [None] * len(available_nodes)
 
 
 def longest_path_incorrect(v):
@@ -20,12 +30,14 @@ def longest_path_incorrect(v):
 
 
 def longest_path(v):
-    max_edge = 0
-    for edge in v[1:]:
-        edge_path = longest_path(available_nodes[edge]) + 1
-        if edge_path > max_edge:
-            max_edge = edge_path
-    return max_edge
+    if not M[v[0]]:
+        max_edge = 0
+        for edge in v[1:]:
+            edge_path = longest_path(available_nodes[edge]) + 1
+            if edge_path > max_edge:
+                max_edge = edge_path
+        M[v[0]] = max_edge
+    return M[v[0]]
 
 
 print(longest_path_incorrect(v1))
