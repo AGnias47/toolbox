1ffa99eec710ff6101f3d4bcbfbbc2b7cb7071b9
Andy Gnias <AGnias47@gmail.com>
2024-03-03 17:28:33 -0500

    First attempt (bad)

--- algorithms/zion_defense_system.py
+++ algorithms/zion_defense_system.py
@@ -12,29 +12,38 @@ def destroy_robots(x, j):
 
 
 def emp_planner(robot_arrivals):
-    for i in range(1, len(robot_arrivals)):
-        print(robot_arrivals[i])
+    n = len(robot_arrivals[1:])
+    if n == 2:
+        return destroy_robots(robot_arrivals[1], 1)
+    j = 1
+    while j <= n and destroy_robots(robot_arrivals[j], j) <= robot_arrivals[j]:
+        j += 1
+    j -= 1
+    robots_destroyed = destroy_robots(robot_arrivals[j], j)
+    if j < n:
+        robots_destroyed += destroy_robots([SENTINEL].extend(robot_arrivals[j + 1 :]))
+    return robots_destroyed
 
 
 def bad_emp_planner(robot_arrivals):
-    robots_destroyed = 0
     n = len(robot_arrivals[1:])
     j = 1
     while j <= n:
         if emp(j) >= robot_arrivals[n]:
-            robots_destroyed += destroy_robots(robot_arrivals[n], n)
+            robots_destroyed = destroy_robots(robot_arrivals[n], n)
             break
         j += 1
     else:
         j -= 1
-        robots_destroyed += destroy_robots(robot_arrivals[n], n)
+        robots_destroyed = destroy_robots(robot_arrivals[n], n)
     if n - j >= 1:
-        robots_destroyed += bad_emp_planner(robot_arrivals[: n - j+1])
+        robots_destroyed += bad_emp_planner(robot_arrivals[: n - j + 1])
     return robots_destroyed
 
 
 if __name__ == "__main__":
-    robot_schedule = [SENTINEL, 1, 10, 10, 1]  # 5
-    print(bad_emp_planner(robot_schedule))
-    robot_schedule = [SENTINEL, 1, 1, 4, 2]  # 5
-    print(bad_emp_planner(robot_schedule))
+    s1 = [SENTINEL, 1, 10, 10, 1]  # 5
+    s2 = [SENTINEL, 1, 1, 4, 2]  # 5
+    for robot_schedule in [s1, s2]:
+        print(bad_emp_planner(robot_schedule))
+        print(emp_planner(robot_schedule))
